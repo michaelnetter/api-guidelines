894f8154a4073877e1217ba8f02dc9eb8386b051
Michael Netter <mail@michaelnetter.com>
2020-04-30 17:11:50 +0200

    finish version 0.9.0

--- script.js
+++ script.js
@@ -4,29 +4,42 @@ new Docute({
   target: '#docute',
   sourcePath: './docs/',
   title: 'API Guidelines',
-  darkThemeToggler: 'true',
-  layout: 'left',
+  layout: 'narrow',
+  detectSystemDarkTheme: true,
+  darkThemeToggler: true,
+  editLinkBase: 'https://github.com/michaelnetter/api-guidelines/tree/master/docs',
+  editLinkText: 'Edit this page on GitHub',
+  router: {
+    mode: 'history'
+  },
+  detectSystemDarkTheme: true,
+  darkThemeToggler: true,
+  versions: {
+    '0.9.0 (Latest)': {
+      link: '/'
+    }
+  },
   nav: [
     {
       title: 'Home',
       link: '/'
     },
     {
-      title: 'About',
-      link: '/about'
+      title: 'API reference',
+      link: 'https://dracoon.team/api'
     }
   ],
   sidebar: [
     {
       title: 'Guide',
-      links: [
+      children: [
         {
           title: 'Introduction',
-          link: '/introduction'
+          link: '/'
         },
         {
-          title: 'Installation',
-          link: '/installation'
+          title: 'Customization',
+          link: '/about'
         }
       ]
     }
